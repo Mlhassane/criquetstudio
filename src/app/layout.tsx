0e9e74c6eae375bf0bee182239415b278011a048
mahamanelhassane <147876518+Mlhassane@users.noreply.github.com>
2025-06-16 02:33:37 +0100

    corrections et mises ajour cotenu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import Navbar from "@/components/navbar";
 import { ReactQueryProvider } from "@/provider/reactquery";
-import Footer from "@/components/Footer";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -35,7 +34,7 @@ export default function RootLayout({
       <ReactQueryProvider>
           <Navbar />
           {children}
-          <Footer />
+          <Footer/>
         </ReactQueryProvider>
       </body>
     </html>
