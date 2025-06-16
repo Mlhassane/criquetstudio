6f786afe20d24c63ad42cccaf3303bbe2f225ac2
mahamanelhassane <147876518+Mlhassane@users.noreply.github.com>
2025-06-16 03:49:56 +0100

    corrections et mises a jour cotenu d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProvider } from "@/provider/reactquery";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
